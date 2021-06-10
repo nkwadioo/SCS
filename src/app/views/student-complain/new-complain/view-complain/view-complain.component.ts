83f56a0a86077f52132a16ba9c1090e4d5d019a7
Tiisetsom <tiisetsomametse@gmail.com>
2021-06-10 17:06:35 +0200

    hjjj

--- src/app/views/student-complain/new-complain/view-complain/view-complain.component.ts
+++ src/app/views/student-complain/new-complain/view-complain/view-complain.component.ts
@@ -20,35 +20,36 @@ export class ViewComplainComponent implements OnInit {
   ResidenceNumber = this.refData.ResidenceNumber;
   ResidenceFloor = this.refData.ResidenceFloor;
 
-  constructor( private fb: FormBuilder,
+  constructor(private fb: FormBuilder,
     public rgx: RGXService,
     public router: Router,
     public refData: ReferenceDataService,
     private dialogs: DialogsService,
     private apiservice: ApiService,
     private loadingService: LoadingService,
-    private session: SessionsService,)
-     { 
+    private session: SessionsService,) {
     this.complainInfor = fb.group({
       resNumber: ['', Validators.required],
       residenceFloor: ['', Validators.required],
       roomNumber: ['', Validators.required],
       comaplainType: ['', Validators.required],
       complainDetails: ['', Validators.required],
-      
+
       studentName: ['', Validators.required],
       studentSurname: ['', Validators.required],
       studentNumber: ['', Validators.required],
       cellphoneNumber: ['', Validators.required],
-      emailAddress: ['', Validators.required],   
+      emailAddress: ['', Validators.required],
     });
   }
+
   ngOnInit() {
+
     if (this.apiservice.referencenumber) {
       this.loadingService.loadingOn();
       this.apiservice
         .getApplicationByReference(
-          `${this.apiservice.userID}/${this.apiservice.AccredAppName}/${this.apiservice.TTAreferencenumber}`
+          `${this.apiservice.userID}/${this.apiservice.complain}/${this.apiservice.referencenumber}`
         )
         .toPromise()
         .then((res) => {
@@ -56,16 +57,16 @@ export class ViewComplainComponent implements OnInit {
             const studentDetails = res.myaap.StudentInformation;
             const complainDetails = res.myaap.ComplainInformation;
             this.refData.ResidenceFloor.filter(item => {
-                if (item.id === complainDetails.residenceFloor) {
-                  complainDetails.residenceFloor = item.Floor;
-                }
+              if (item.id === complainDetails.residenceFloor) {
+                complainDetails.residenceFloor = item.Floor;
+              }
             });
             this.refData.ResidenceNumber.filter(item => {
               if (item.id === complainDetails.resNumber) {
                 complainDetails.resNumber = item.Res;
               }
-          });
-   // after filtering
+            });
+            // after filtering
             this.complainInfor = this.fb.group({
               studentName: [studentDetails.studentName, Validators.required],
               studentSurname: [studentDetails.studentSurname, Validators.required],
@@ -84,6 +85,10 @@ export class ViewComplainComponent implements OnInit {
         });
       this.loadingService.loadingOff();
     }
+  }
+
+  manageComplain() {
+    this.router.navigate(['/student-complain/Manage']);
   }
 
 }
